4bf521a25c2cc715abc63761fa9e8f4c1f7dd79b
Lirim <email@domain.com>
2026-04-03 15:10:34 +0200

    Add Netlify redirects fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/notes/" : "/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
